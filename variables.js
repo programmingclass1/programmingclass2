a550322e5b557279881a76671bf11623159d441f
stephenJ <mrsteveglenn@gmail.com>
2016-09-23 15:38:13 +0000

    arron changes

---
+++ variables.js
@@ -0,0 +1,53 @@
+
+var myvaribale = 324;
+
+console.log(myvaribale);
+ 
+myvaribale = 100;
+
+console.log(myvaribale);
+
+var a = 5;
+
+var b = 2;
+
+var c = a + b;
+
+console.log(c);
+
+c = a * b;
+
+console.log(c);
+
+c = a / b;
+
+console.log(c);
+
+// integer
+var r = 1;
+
+// float, double
+var fl = 1.0;
+
+// boolean
+var t = true;
+var fa = false;
+
+
+var myString = "asdfasdf";
+var myString1 = "hoffman";
+
+console.log(myString + myString1);
+// asdfasdfhoffman
+
+
+var dt = new Date(22343232);
+//dt = @"2015-10-14"
+
+
+var myArray = [1, 2, 4, 5];
+
+// access first item with a 0 -- NOT the number 1
+var firstItem = myArray[0];
+
+
